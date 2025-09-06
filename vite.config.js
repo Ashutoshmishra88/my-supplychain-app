d4312ad4cf9287a9addf0e3a949b7c14f49d3daf
Ashutosh Mishra <61501971+Ashutoshmishra88@users.noreply.github.com>
2025-09-07 01:15:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/my-supplychain-app/",
   plugins: [react()],
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
